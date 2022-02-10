7acaffaaa534e60269fb7468afd8b5bc473ed882
rrryuutei <48124924+rrryuutei@users.noreply.github.com>
2022-02-09 18:24:26 -0800

    Update Question6.java

--- assignment1/src/Question6.java
+++ assignment1/src/Question6.java
@@ -5,18 +5,24 @@ public class Question6 {
         Scanner in = new Scanner(System.in);
         System.out.println("Please input your monthly calls:");
         int number = in.nextInt();
-        double bills = 0;
-        if( number<=100 ) {
-            bills = 200;
-        }
-        else if( number>100&&number<=150 ) {
-            bills = 200+(number-100*0.6);
-        }
-        else if( number>150&&number<=200 ) {
-            bills = 200+(number-150*0.5);
-        }
-        else {
-            bills = 200+(number-200*0.4);
+        double bills = 200;
+        int flag=0;
+        if(number>=100 && number<=150) flag=1;
+        else if(number<=200) flag=2;
+        else flag=3;
+        switch(flag)
+        {
+            case 0:
+                break;
+            case 1:
+                bills+=(number-100)*0.6;
+                break;
+            case 2:
+                bills+=(number-150)*0.5+50*0.6;
+                break;
+            case 3:
+                bills+=50*0.5+50*0.6+(number-200)*0.4;
+                
         }
         System.out.println("The monthly telephone bills is  "+bills);
     }
