41bf0d77364f3da07474a4761897d955dbfe3c9a
rrryuutei <48124924+rrryuutei@users.noreply.github.com>
2022-02-19 16:36:03 -0800

    Update person.java

--- assignment2/src/person.java
+++ assignment2/src/person.java
@@ -1,20 +1,30 @@
-public class person {
+class Person {
+ private String name;
+ private int ID;
+ private int age;
+ 
+ public Person(String name,int ID,int age) {
+  this.name = name;
+  this.ID = ID;
+  this.age = age;
+ }
+ 
+ @Override
+ public String toString() {
+  return "Person [name=" + name + ", ID=" + ID + ", age=" + age + "]";
+ }
+ 
+}
 
-    public class person  {
-        String Name;
-        int ID;
-        int Age;
+class Professor extends Person{
 
-        public String getName() {
-            return Name;
-        }
-
-        public  class  person { String name; int age; int ID};
-        this
-
-        this
-
-        public int getID() {
-
-        }
-    }
+ public Professor(String name, int ID, int age) {
+  super(name, ID, age);
+  // TODO Auto-generated constructor stub
+ }
+ 
+ public String giveQuiz() {
+  return super.toString();
+ }
+ 
+}
