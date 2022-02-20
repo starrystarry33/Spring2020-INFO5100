cb8737ec29b14ecdccfac4a72ffda31ee8e6ed4b
starrystarry33 <98726282+starrystarry33@users.noreply.github.com>
2022-02-19 19:33:36 -0500

    Update student.java

--- assignment2/src/student.java
+++ assignment2/src/student.java
@@ -1,4 +1,7 @@
-public class student {
+public class student extends Person{
+        
+        public Student(String name, int age, int id){
+                supper(name,age,id);
 
         @Override
         public String toString() {
